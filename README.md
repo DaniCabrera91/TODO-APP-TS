1efb1298540269be7d985db773bf5136f7fa3104
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2025-01-23 18:19:57 +0100

    done al the Readme steps, only rest 3 of them

--- README.md
+++ README.md
@@ -5,14 +5,13 @@
 - [x] Estilos del TODO MVC
 - [x] Listar todos los TODOs
 - [x] Poder borrar un TODO
-- [] Marcar un TODO como completado 
-- [] Añadir forma de filtrar TODOs (Footer)
-- [] Mostrar número de TODOs pendientes (Footer)
-- [] Añadir forma de borra todos los TODOs completados 
-- [] Crear un Header con input (Header) 
-- [] Crear un TODO (Header)
-- [] Poder editar el teto de un TODO (Double Click)
+- [x] Marcar un TODO como completado
+- [x] Añadir forma de filtrar TODOs (Footer)
+- [x] Mostrar número de TODOs pendientes (Footer)
+- [x] Añadir forma de borra todos los TODOs completados
+- [x] Crear un Header con input (Header)
+- [x] Crear un TODO (Header)
+- [] Poder editar el texto de un TODO (Double Click)
 - [] Añadir animaciones (AutoAnimate)
 - [] Pasa r a Reducer
 - [] Sincronizar con el Backend
-
--- todo-app-ts/src/components/Todos.tsx
+++ todo-app-ts/src/components/Todos.tsx
@@ -1,26 +1,34 @@
-import { TodoId, type ListOfTodos } from "../types"
-import { Todo } from "./Todo"
+import { type Todo as TodoType, TodoId, type ListOfTodos } from "../types";
+import { Todo } from "./Todo";
 
 interface Props {
-    todos: ListOfTodos
-    onRemoveTodo: ({ id }: TodoId) => void
+  todos: ListOfTodos;
+  onToggleCompleteTodo: ({
+    id,
+    completed,
+  }: Pick<TodoType, "id" | "completed">) => void;
+  onRemoveTodo: ({ id }: TodoId) => void;
 }
 
-export const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
-    return(
-        <ul className="todo-list">
-            {todos.map(todo => (
-                <li key={todo.id} 
-                    className={`${todo.completed ? 'completed' : ''}`}>
-                    <Todo
-                        key={todo.id}
-                        id={todo.id}
-                        title={todo.title}
-                        completed={todo.completed}
-                        onRemoveTodo={onRemoveTodo}
-                    />
-                </li>
-            ))}
-        </ul>
-    )
-} 
+export const Todos: React.FC<Props> = ({
+  todos,
+  onRemoveTodo,
+  onToggleCompleteTodo,
+}) => {
+  return (
+    <ul className="todo-list">
+      {todos.map((todo) => (
+        <li key={todo.id} className={`${todo.completed ? "completed" : ""}`}>
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+            onToggleCompleteTodo={onToggleCompleteTodo}
+            onRemoveTodo={onRemoveTodo}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
